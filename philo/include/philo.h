2e39b18f939cb4c8f3079d9002cf4e9e345288b9
viodid <david.yunta.aller@gmail.com>
2024-12-23 10:39:39 +0100

    check input args

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 19:55:10 by dyunta            #+#    #+#             */
-/*   Updated: 2024/11/14 19:56:13 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/12/23 10:09:59 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct timeval t_timeval;
+
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 19:47:26 by dyunta            #+#    #+#             */
-/*   Updated: 2024/12/23 09:52:21 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/12/23 10:36:46 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,40 @@ struct s_philosopher{
 
 }	t_philosopher;
 
-typedef struct timeval t_timeval;
+static u_int8_t	ft_is_not_num(const char *str)
+{
+	while (*str)
+	{
+		if (*str < 48 || *str > 57)
+			return (EXIT_FAILURE);
+		str++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+u_int8_t	parse_arguments(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc < 5 || argc > 6)
+		return (EXIT_FAILURE);
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_is_not_num(argv[i]))
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
 
 int	main(int argc, char *argv[])
 {
-	for (int i = 0; i < 20; i++) {
-		t_timeval	epoch;
-		gettimeofday(&epoch, NULL);
-		printf("tv_sec: %ld - tv_usec: %ld\n", epoch.tv_sec, epoch.tv_usec);
-		usleep(100000);
+	if (parse_arguments(argc, argv))
+	{
+		write(2, "Usage: ./philo [no_philo] [time_die] ", 37);
+		write(2, "[time_eat] [time_sleep] (must_eat)\n", 36);
+		return (EXIT_FAILURE);
 	}
-
 	return (EXIT_SUCCESS);
 }
